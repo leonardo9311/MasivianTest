9ab6b702b98e849f48b555d49c6ebebd696e5f2f
leonardo garcia <garcial_e@javeriana.edu.co>
2021-07-27 10:54:19 -0500

    funcitonaltest

---
+++ MasivianPrueba.FunctionalTest/Web/RouletteControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasivianPrueba.FunctionalTest.Web
+{
+    [TestFixture]
+    public class RouletteControllerTEst
+    {
+        public HttpClient Client { get; }
+        public RouletteControllerTEst()
+        {
+            WebTestFixture factory = new WebTestFixture();
+            Client = factory.CreateClient();
+        }
+      
+    }
+}
